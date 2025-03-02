6849ceb5ba1a5a96fd43a739a761ea43b2ff3548
vladislavbadan <vlad.badan@yandex.ru>
2025-03-02 16:44:13 +0300

    Добавлены флаги

--- file_manager.go
+++ file_manager.go
@@ -1,40 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
+// функция создания файла
+func createFile(filename string, text string) {
+	file, err := os.Create(filename)
+
+	if err != nil {
+		fmt.Println("Не получилось создать:", err)
+		os.Exit(1)
+	}
+
+	defer file.Close()
+	writeFile(text, file)
+}
+
+// функция записи файла
+func writeFile(text string, file *os.File) {
+	file.WriteString(text)
+}
+
+// размер файла
+func sizeFile(filename string) {
+	f, err := os.Open(filename)
+
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("File size:", fi.Size(), "bytes")
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Введи: go run file_manager.go <ИМЯ ФАЙЛА>")
+	// Определяем флаги
+	createFlag := flag.Bool("create", false, "Create file if it doesn't exist")
+	sizeFlag := flag.Bool("size", false, "Print file size")
+	textFlag := flag.String("text", "Hello, Go!", "Text to write to the file")
+
+	flag.Parse()
+
+	// Получаем имя файла из аргументов
+	filename := flag.Arg(0)
+	if filename == "" {
+		fmt.Println("Filename is required")
 		return
 	}
 
-	filename := os.Args[1]
+	if *createFlag {
+		createFile(filename, *textFlag)
+	}
 
-	// открываем файл
-	f, err := os.Open(fmt.Sprintf("%s.txt", filename))
-	// проверка на ошибка
-	if os.IsNotExist(err) {
-		// создаем файл так как его нет
-		file, err := os.Create(fmt.Sprintf("%s.txt", filename))
-
-		if err != nil {
-			fmt.Println("Не получилось создать:", err)
-			os.Exit(1)
-		}
-		defer file.Close()
-		file.WriteString("Hello go!")
-
-	} else {
-		defer f.Close()
-
-		fi, err := f.Stat()
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println("File size:", fi.Size(), "bytes")
+	if *sizeFlag {
+		sizeFile(filename)
 	}
 }
